bc893342e206a690eff52952ebe1b4eac2b10d9b
lee <linkedlinked@gmail.com>
2011-06-22 14:32:40 -0700

    admin user invites

--- easy/signup_codes/urls.py
+++ easy/signup_codes/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 
 
 patterns = patterns("",
+    url(r"^admin/$", "easy.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     #url(r"^failure/$", )
 )
