a8b7962ceb8f92931e9de0561a8b493bd3b7f109
lee <linkedlinked@gmail.com>
2011-06-22 13:58:02 -0700

    fixing an import issue

--- easy/waitinglist/models.py
+++ easy/waitinglist/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from easy.models import easy_model
 
-
-
 class WaitingListEntry(easy_model):
     email = models.EmailField(_("email address"), unique=True)
     created = models.DateTimeField(_("created"), default=datetime.now, editable=False)
